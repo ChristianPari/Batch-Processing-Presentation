bec54754a0e39b9e69a64938bf375173a76a110d
Christian Pari <christianpari@outlook.com>
2021-02-04 19:47:15 -0500

    Created a new SpringBoot project

---
+++ batch-processing-walkthrough/src/main/java/com/christianpari/BatchWalkthroughApplication.java
@@ -0,0 +1,13 @@
+package com.christianpari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchWalkthroughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchWalkthroughApplication.class, args);
+	}
+
+}
